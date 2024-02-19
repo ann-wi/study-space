bcd44d3af4f4e8a7fa3d88d2f915d9af767173e6
Anya <vann2299@yandex.ru>
2024-02-19 09:34:43 +0300

    added routes for basic pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
-// import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './pages/about';
+import Home from './pages/home';
+import Dashboard from './pages/dashboard';
+import TodoList from './pages/todo-list';
+import Resources from './pages/resources';
+import NotFound from './pages/not-found';
 
 export default function App() {
   return (
-    <h1 className="text-3xl text-red-300">App Study Space</h1>
+     <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/todolist' element={<TodoList />} />
+        <Route path='/resources' element={<Resources />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+     </BrowserRouter>
   )
 }
---
+++ client/src/pages/not-found.jsx
@@ -0,0 +1,5 @@
+export default function NotFound() {
+  return (
+    <div>NOTFOUND 404</div>
+  )
+}
